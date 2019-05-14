075c079d5a987166de9cdf4bcb87dd5fd87ce39e
Abhinav Manchanda <abhinav@abhinav.local>
2019-05-14 16:51:16 +0530

    Workshop sends an alert to all registered notificationreceivers.

--- src/main/java/com/example/factory/Workshop.java
+++ src/main/java/com/example/factory/Workshop.java
@@ -1,10 +1,22 @@
 package com.example.factory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Workshop {
+
+    private List<NotificationReceiver> receivers;
+
+    public Workshop() {
+        receivers = new ArrayList<>();
+    }
 
     public void sendAlert(Alert alert) {
 
-
+        this.receivers.forEach(r -> r.alert(alert));
     }
 
+    public void subscribe(NotificationReceiver notificationReceiver) {
+        this.receivers.add(notificationReceiver);
+    }
 }
--- src/test/java/com/example/factory/WorkshopTest.java
+++ src/test/java/com/example/factory/WorkshopTest.java
@@ -1,5 +1,19 @@
 package com.example.factory;
 
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 public class WorkshopTest {
-   
+
+    @Test
+    public void shouldRaiseAlertToAllAlertables(){
+        Workshop workshop = new Workshop();
+        final NotificationReceiver notificationReceiver = mock(NotificationReceiver.class);
+        workshop.subscribe(notificationReceiver);
+        final Alert alert = new Alert("message", Severity.INFO);
+        workshop.sendAlert(alert);
+        verify(notificationReceiver).alert(alert);
+    }
 }
